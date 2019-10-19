ee8030539f27eca98b9878967b9aa87fc99a1d15
Nhan <d@gmail.com>
2019-10-19 14:07:52 +0700

    upcode

--- src/app/cart-body/cart-body.component.ts
+++ src/app/cart-body/cart-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Product } from "../product.model";
 @Component({
   selector: "app-cart-body",
@@ -7,13 +7,20 @@ import { Product } from "../product.model";
 })
 export class CartBodyComponent {
   @Input() products: Product[];
+  @Output() onChangeQuantity = new EventEmitter();
+  numberItem: number;
   removeProduct(id: number) {
     const index = this.products.findIndex(product => product.id);
     if (index !== -1) {
       this.products.splice(index, 1);
+      this.numberItem = 0;
+      for (let i = 0; index < this.products.length; i++) {
+        this.numberItem += this.products[i].quantity;
+      }
     }
   }
-  changeQuantity(value: string) {
-    console.log(value);
+  changeQuantity(value: string,product : Product) {
+    product.quantity = parseInt(value);
+    this.onChangeQuantity.emit(this.products);
   }
 }
--- src/app/cart-price/cart-price.component.ts
+++ src/app/cart-price/cart-price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-cart-price",
@@ -7,8 +7,14 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class CartPriceComponent {
   promoCode: any;
-  
+
   @Input() Subtotal: number;
   @Input() Tax: number;
   @Input() Total: number;
+
+  @Output() onApplyPromoCode = new EventEmitter();
+
+  applyPromoCode() {
+    this.onApplyPromoCode.emit(this.promoCode);
+  }
 }
